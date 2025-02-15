99988736cb79bf50da67fc9efd8a958751dd9cbd
Aleksei Laptev <mar.d.soul.app@gmail.com>
2025-02-16 01:09:23 +0400

    add tests

---
+++ src/test/java/study/test/Pair.java
@@ -0,0 +1,20 @@
+package study.test;
+
+class Pair<K, V> {
+
+    private final K first;
+    private final V second;
+
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public K getFirst() {
+        return this.first;
+    }
+
+    public V getSecond() {
+        return this.second;
+    }
+}
